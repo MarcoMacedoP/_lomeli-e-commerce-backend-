dc40f53ce36bcc4b67e31d2c80036a455748260e
Marco <marcosiegman01@gmail.com>
2019-12-04 14:54:25 -0600

    added database

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,10 +1,11 @@
 'use strict'
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
+
+const Database = use('Database')
 
 class User extends Model {
   static boot () {
@@ -33,6 +34,9 @@ class User extends Model {
    */
   tokens () {
     return this.hasMany('App/Models/Token')
+  }
+  create(){
+    Database.table('users').insert()
   }
 }
 
