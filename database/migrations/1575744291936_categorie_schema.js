966cfcbbb41c4aaa84c422895a9e85d87344db3c
Marco <marcosiegman01@gmail.com>
2019-12-07 12:55:08 -0600

    added migration for categories

---
+++ database/migrations/1575744291936_categorie_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CategorieSchema extends Schema {
+  up() {
+    //prettier-ignore
+    this.create('categories', table => {
+      table.increments()
+      table.integer('parent_id')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('name', 100).notNullable()
+      table.enu('status', ['active', 'inactive']).defaultTo('active').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('categories')
+  }
+}
+
+module.exports = CategorieSchema
