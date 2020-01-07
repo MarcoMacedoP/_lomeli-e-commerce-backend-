6ddf8f05dddd5b176422e112d642d6a69f9c7a26
Macedo <marcosiegman01@gmail.com>
2020-01-07 10:18:42 -0600

    created wishlist after client signup

--- app/Models/Clients/Client.js
+++ app/Models/Clients/Client.js
@@ -4,10 +4,25 @@
 const Model = use('Model')
 /** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
+const Wishlist = use('App/Models/Clients/Wishlist')
 
 class Client extends Model {
   static boot() {
     super.boot()
+
+    /**A hook to create the client wishlist after user is created*/
+    this.addHook('afterCreate', async client => {
+      await Wishlist.create({
+        client_id: client.id
+      })
+    });
+    /**
+     * A hook to remove the password value before return it to server
+     */
+    this.addHook('afterSave', client => {
+      delete client['$attributes'].password
+      return client
+    })
 
     /**
      * A hook to hash the user password before saving
